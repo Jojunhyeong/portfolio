bfbac942cfb42e011480554d4e81e50de8bff537
jojunhyeong <jojh0323@naver.com>
2025-04-20 12:58:05 +0900

    [fix] Add top margin to about Add animation to skillcard Add description to techstack

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,16 +1,17 @@
 import { IconType } from "react-icons";
 
 interface SkillCardProps {
-  name: string;
-  icon: IconType;
-  color: string;
+    name: string;
+    icon: IconType;
+    color: string;
 }
 
-export default function SkillCard({ name, icon: Icon, color}: SkillCardProps) {
+export default function SkillCard({ name, icon: Icon, color }: SkillCardProps) {
     return (
-        <div className="bg-gray-50 rounded-xl p-6 shadow hover:shadow-md transition text-center flex items-center">
-            <Icon className={`text-4xl ${color} mb-2 ml-14`}/>
+        <div className="bg-gray-50 rounded-xl p-6 shadow transition-all duration-200 transform hover:-translate-y-1 hover:scale-105 hover:shadow-md text-center flex items-center">
+            <Icon className={`text-4xl ${color} mb-2 ml-14`} />
             <p className="text-lg font-semibold text-gray-700 ml-10">{name}</p>
         </div>
+
     )
 }
--- src/sections/TechStack.tsx
+++ src/sections/TechStack.tsx
@@ -32,6 +32,7 @@ export default function TechStack() {
     return(
         <section id="tech" className="w-full bg-gray-50  py-20 px-6">
             <h2 className="text-3xl md:text-4xl font-bold text-gray-800 mb-12 text-center">기술 스택</h2>
+            <h3 className="text-2xl md:text-xl font-semibold text-gray-600 mb-12 text-center">다양한 프론트엔드 기술을 활용하여 사용자 친화적인 웹 애플리케이션을 개발합니다.</h3>
 
             <div className="max-w-5xl mx-auto mb-16">
                 <h3 className="text-2xl font-semibold text-gray-700 mb-6">Frontend</h3>
